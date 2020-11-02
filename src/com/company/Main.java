9e5a0e3f9768562adc8badb1b0ccacd1d955d426
JessicaQueiroz <jehqueiroz01@gmail.com>
2020-11-02 20:38:07 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int numero = 0;
+	    do{
+	        System.out.println(numero);
+	        numero += 1;
+        } while(numero < 5);
+    }
+}
